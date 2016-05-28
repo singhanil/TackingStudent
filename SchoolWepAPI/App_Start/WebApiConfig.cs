32ed9c2ee00d5e73c5412e50c4d5faaa783cb19e
ARLahoti <akashlahoti1983@gmail.com>
2016-05-28 22:33:39 +0530

    install nugate package for cross domain

--- SchoolWepAPI/App_Start/WebApiConfig.cs
+++ SchoolWepAPI/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Web.Http.Cors;
 using System.Net.Http.Headers;
 using System.Web.Http;
 
@@ -12,7 +13,7 @@ namespace SchoolWepAPI
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-
+            EnableCrossSiteRequests(config);
             // Web API routes
             config.MapHttpAttributeRoutes();
 
@@ -25,6 +26,14 @@ namespace SchoolWepAPI
 
             config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
             //config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/json"));
+        }
+        private static void EnableCrossSiteRequests(HttpConfiguration config)
+        {
+            var cors = new EnableCorsAttribute(
+                origins: "*",
+                headers: "*",
+                methods: "*");
+            config.EnableCors(cors);
         }
     }
 }
