400d908fedc120401a93382f14bcd752d5249269
ARLahoti <akashlahoti1983@gmail.com>
2016-06-25 11:41:31 +0530

    Dashbord report search implemented

--- StudentTraking.AngularWeb/TrakingStudent/App/Services/DashBoardService.js
+++ StudentTraking.AngularWeb/TrakingStudent/App/Services/DashBoardService.js
@@ -4,15 +4,17 @@
     var DashBoardService = function ($http, $rootScope, Constant, $state) {
         var service = {};
         var _constant = new Constant();
-        var getDailyStudentReport = function () {
+        var getDailyStudentReport = function (classId, sectionId) {
             var APIURL = _constant.get("studenttrakingurl");
             var url = APIURL + _constant.get("getstuddailyreportlurl") + "/" + $rootScope.User.SecurityToken + "/" + $rootScope.User.SchoolId;
-
+            if (classId != undefined && sectionId != undefined)
+                url = url + '/' + classId + '/' + sectionId;
             return $http({
                 url: url,
                 method: "GET"
             })
         };
+        
         service.getDailyStudentReport = getDailyStudentReport;
         return service;
     };
