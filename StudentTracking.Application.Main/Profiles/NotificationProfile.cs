5127ef6886b6cda788ce5f947548b461ae513fbb
ARLahoti <akashlahoti1983@gmail.com>
2017-02-25 16:30:03 +0530

    added class for notification

---
+++ StudentTracking.Application.Main/Profiles/NotificationProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using StudentTracking.Application.Models;
+using StudentTracking.Data;
+
+namespace StudentTracking.Application.Main.Profiles
+{
+    public class NotificationProfile : ProfileBase
+    {
+        protected override void Configure()
+        {
+            Mapper.CreateMap<Data.Notification, Models.NotificationNew>()
+                .ForMember(model => model.MessageId, map => map.Ignore())
+                .ForMember(model => model.SchoolId, map => map.Ignore());
+
+            Mapper.AssertConfigurationIsValid(typeof(NotificationProfile).Name);
+        }
+    }
+}
